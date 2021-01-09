3acc4ce94567fea3f764398448aa01004c2fa6ca
99003161 <75414474+99003161@users.noreply.github.com>
2021-01-09 22:15:21 +0530

    Create count.h

---
+++ Assignments/4/count.h
@@ -0,0 +1,13 @@
+#ifndef COUNT_H_INCLUDED
+#define COUNT_H_INCLUDED
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+
+
+#endif // COUNT_H_INCLUDED

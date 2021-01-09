c3a74d564cb0fec91fb68c5e723a5f5afda51a96
99003161 <75414474+99003161@users.noreply.github.com>
2021-01-09 17:17:25 +0530

    Create cmp.h

---
+++ Assignments/3/cmp.h
@@ -0,0 +1,13 @@
+#ifndef CMP_H_INCLUDED
+#define CMP_H_INCLUDED
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+
+
+#endif // CMP_H_INCLUDED

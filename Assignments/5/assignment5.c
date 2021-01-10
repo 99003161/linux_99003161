3009d79c9a3dd5366458ce5d86079cd8363e3f81
99003161 <75414474+99003161@users.noreply.github.com>
2021-01-10 11:43:48 +0530

    Update assignment5.c

--- Assignments/5/assignment5.c
+++ Assignments/5/assignment5.c
@@ -1,30 +1,17 @@
-#include<sys/wait.h>
-#include<stdio.h>
-#include<unistd.h>
-#include<operations.h>
+#include "cmpl.h"
 
 
 int main()
 {
-int temp,status,c=0;
+int temp,status;
 temp=fork();
 if(temp<0)
 {
 printf("error");
 }
-else if(temp==0 && c==0)
+else if(temp==0)
 {
 execlp("gcc","gcc","sum1.c","-o","su1",NULL);
-exit(0);
-}
-else if(temp==0 && c==1)
-{
-execlp("gcc","gcc","sum1.c","-o","sb1",NULL);
-exit(0);
-}
-else if(temp==0 && c==2)
-{
-execlp("gcc","gcc","sum1.c","-o","mu1",NULL);
 exit(0);
 }
 else

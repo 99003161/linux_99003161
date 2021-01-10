2aca3304524d45034c6a6d20859959d2ee2eacfd
99003161 <75414474+99003161@users.noreply.github.com>
2021-01-10 11:12:26 +0530

    Update assignment7.c

--- Assignments/7/assignment7.c
+++ Assignments/7/assignment7.c
@@ -1,16 +1,5 @@
-#include<stdio.h>
-#include<pthread.h>
-void* sum_fun(void* sub_array)
-{
-    int* partarray = (int*)sub_array;
-    int sum=0;
-    for(int i=0;i<10;i++)
-    {
-        sum+=partarray[i];
-    }
-    pthread_exit((void*)sum);
+#include "sumarr.h"
 
-}
 int main()
 {
     int arr[100];
